274481008fdbf2a01bf0b2d138d36339658e171d
hustacdjm <hustacdjm@gmail.com>
2024-03-26 20:37:09 -0700

    initial react component base

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import YouTube from 'react-youtube';
 
-class App extends React.Component {
-
-  render() {
+const App = ({data, onUpdateData}) => {
+  
     const reactVersion = require('./package.json').dependencies['react'];
     
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
-    };
-
     return ([
         
-        <h1>
+        <h1 key='h1key'>
           React
           <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="30"></img>
         </h1>,
 
-        <p>
+        <p key='pkey'>
           React Version: {reactVersion}
-        </p>,        
-        
-        <YouTube videoId="2g811Eo7K8U" opts={opts}  />        
+        </p>      
+         
     ])
-  }
+  
 }
 
 class Mfe5Element extends HTMLElement {
-  connectedCallback() {
-    ReactDOM.render(<App/>, this);
+  
+  constructor(){
+    super();
+    this.dataState = null;
   }
+
+  getDataState(){
+    return this.dataState;
+  }
+
+  setDataState(dataState){
+    this.dataState = dataState;
+    console.log("react component set DataState:");
+    console.log(this.dataState);
+  }
+  
+  connectedCallback() {
+
+    let data = this.getAttribute("data");
+    console.log("react component input data");
+    console.log(data);
+
+    ReactDOM.render(<App data={data} onUpdateData={this.setDataState}/>, this);
+  }
+
+
 }
 
 customElements.define('react-component6601e239be361e17e019bbb5', Mfe5Element);
