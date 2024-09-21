2c2abd8cd0505981736a2ec8bc94788b6ca411fc
hustacdjm <hustacdjm@gmail.com>
2024-09-20 19:40:16 -0700

    add score ability

--- react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493.js
+++ react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493.js
@@ -1,16 +1,44 @@
 import React from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { fetchScore } from './score';
 
-const App = ({data, onUpdateData}) => {
+const App = ({data, env, onUpdateData}) => {
   
-   
+    const [scoreResponse, setScoreResponse] = useState(null);
+
+    const handleScoreComponent = ()=>{
+
+      console.log('Start scoring...');
+
+      fetchScore(data, env)
+            .then(scoreResponse => {
+                 console.log(scoreResponse);
+                setScoreResponse(scoreResponse);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });      
+    }
+
+    
     return (
-      
+      <>
         <div className="text-3xl mt-4 font-bold underline">
-         Develop the React Component
+         Develop the React Component <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" onClick={handleScoreComponent}>Score</button>
         </div>
-      
+
+        <div>       
+          {scoreResponse && (
+            <div>
+              <h3>Score Response:</h3>
+              <pre>{JSON.stringify(scoreResponse.runtime.score, null, 2)}</pre>
+            </div>
+          )}
+          In this approach:
+      </div>
+      </>
     )
   
 }
@@ -35,20 +63,49 @@ class Elementreact660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493 exten
   connectedCallback() {
 
     let data = this.getAttribute("data");
+    let evn = this.getAttribute("env");
 
     let dataJson = null;
+    let envJson = null;
     if(data==null){
       //Test Mode
-      dataJson={};
+      dataJson={
+        "component" : {
+          "componentTemplateId": "30a70399b4ed499f9eb8208993b7165c",
+          "templateDeployId": null,
+          "content" : {
+            "question" : "This is the question?"
+          },
+          "ui" : { }
+        },
+        "runtime" : {
+          "data" : {
+            "answer" : "people"
+          }
+        },
+        "constants" : { }
+      };
     }
     else{
       dataJson = JSON.parse(data);
     }
 
+    if(envJson==null){
+      envJson={
+        'Authorized': 'X-Asynmous-User-ID',
+        'ScoreUrl': 'http://localhost:8081/v3/edu/ReactiveComponentRuntime/score'
+      }
+    }
+    else{
+      envJson=JSON.parse(env);
+    }
+
+
+
     console.log("react component input data");
     console.log(dataJson);
 
-    ReactDOM.render(<App data={dataJson} onUpdateData={this.setDataState}/>, this);
+    ReactDOM.render(<App data={dataJson} env={envJson} onUpdateData={this.setDataState}/>, this);
   }
 
 
---
+++ score.js
@@ -0,0 +1,35 @@
+export const fetchScore = (data, env) => {
+
+    console.log(env);
+
+    let headers = {
+        'Content-Type': 'application/json',  // Example header
+        'Authorization':`Bearer ${env.Authorized}`
+      };
+  
+      if(!env.Authorized || env.Authorized==='X-Asynmous-User-ID' ){            
+        headers = {
+            'Content-Type': 'application/json',  // Example header
+            'X-Asynmous-User-ID':'X-Asynmous-API-ID'
+          };            
+      }
+  
+      
+
+    return fetch(env.ScoreUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        console.log(response);
+        return response.json();
+    })
+    .catch(error => {
+        console.error("Error fetching score:", error);
+        throw error;
+    });
+};
