bbc1c2edc8bf6f7599b4ccb1d87a7f2fa9f86bdf
hustacdjm <hustacdjm@gmail.com>
2025-02-10 10:55:15 -0700

    react version

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import React from "react";
+
 import('./react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493');
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("react-version").textContent = React.version;
+});
