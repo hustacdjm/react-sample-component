0320ea10cd7c8d729c105b49a7f6988102fe2fa3
hustacdjm <43550193+hustacdjm@users.noreply.github.com>
2025-07-21 15:37:06 -0700

    Update react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493.js
    remove score.component no exist

--- react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493.js
+++ react660394d5be361e17e019bbb953b4d7602e2b45629777bc72f515f493.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import ScoreComponent from './score.component'
 
 const App = ({data, env, onUpdateData}) => {
     
